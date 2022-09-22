aa294d93f6c5d2339bd5f719f1877545c2b86d3b
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-22 07:29:51 +0530

    function basics

--- function.js
+++ function.js
@@ -1,15 +1,5 @@
-function getsum(num1,num2) {
-    total = num1 + num2;
-    //return total;
-    console.log(total);
+function greet(name="user") { //Default parameters
+    console.log("welcome",name);
 }
-getsum(2,3);//arguments = [2,3] // calling a function
-getsum(4,5);
-//1. The name of the function should be in camel case.
-//2. Then name of the function shoulde be explict.
-//3. The name of the function should be  imperative/action based.
-
-
-//function <name of function> (parameters) {
-//body of the function
-//}
+greet ("Berlin");// calling a function
+greet();
