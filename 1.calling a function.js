046caefaf88064527b094ca8e3a8a8683e699660
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-27 15:45:19 +0530

    calling a function

--- 1.calling a function.js
+++ 1.calling a function.js
@@ -2,3 +2,15 @@ function greet(){
     console.log("hello there!");
 }
 greet();//hello there!
+
+
+
+function sayThanks(){
+    console.log ('Thank you for your purchase! We appreciate your business.') ;
+  }
+  sayThanks() ;
+  sayThanks() ;
+  sayThanks() ;
+//   output->Thank you for your purchase! We appreciate your business.
+//   Thank you for your purchase! We appreciate your business.
+//   Thank you for your purchase! We appreciate your business.
