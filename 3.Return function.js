b70e59c0bd1970e55fb8ea0d640a12cc3acb8bd7
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-27 17:32:34 +0530

    Return statement

---
+++ 3.Return function.js
@@ -0,0 +1,27 @@
+function add(a,b){
+    return (a+b)
+}
+
+console.log(add(10,20));
+console.log(add(15,25));
+console.log(add(30,35));
+
+
+function rectangleArea(width, height) {
+    return area = width * height;
+  }
+  var x = rectangleArea(5,7);
+  console.log(x); //35
+  
+
+  
+
+function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  const numOfMonitors = monitorCount(5, 4);
+  
+  console.log(numOfMonitors);//20
+  
+  
