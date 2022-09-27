c1560f46a6c153a794d798ebf5304202815cc4fe
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-27 19:36:09 +0530

    Array Function

---
+++ 6.Arrow Function.js
@@ -0,0 +1,19 @@
+const rectangleArea = (width, height) => {
+    let area = width * height;
+    return area;
+  };
+  console.log(rectangleArea(12,10));//120
+
+
+
+  //Singleline Block ->
+
+const squareNum1 = num => num * num;
+console.log(squareNum1(15));//225
+
+//Multiline Block ->
+const squareNum2 = (num) => {
+return num * num;
+} 
+console.log(squareNum2(15));//225
+
